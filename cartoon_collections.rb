dab379ce4a45ae9f124eec7b25aeb44140fb08f8
Dean L Betty <deanlloyd64@gmail.com>
2019-09-12 03:50:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,5 +22,7 @@ end
 def find_the_cheese(list)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  list.include?cheese_types.any?
+  list.include? do |item|
+    item == cheese_types.any?
+  end
 end
